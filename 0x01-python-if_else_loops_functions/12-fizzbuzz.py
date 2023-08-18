025c9bbe3e967c00d01fc608f4358b73dda40977
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-18 16:58:28 -0700

    check this

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3) == 0 and (number % 5) == 0:
+            print("FizzBuzz", end=" ")
+            continue
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
+            continue
+        else:
+            print("{}".format(number), end=" ")
