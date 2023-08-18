9aeb2071d1ee01cca83139faaf1570228810508e
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-18 15:09:08 -0700

    check this

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(00, 100):
     if x < 99:
-        print("{0:02d}".format(x), end=",")
+        print("{0:02d}".format(x), end=", ")
     else:
         print("{}".format(x))
