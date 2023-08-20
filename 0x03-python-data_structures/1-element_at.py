c3941a0c626b97b738fcc72261951470b6b790b1
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-20 02:30:35 -0700

    check o

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx == 0 or idx > len(my_list):
+    if idx < 0 or idx >= (len(my_list)):
+        print("hello")
         return None
     else:
         return(int(my_list[idx]))
