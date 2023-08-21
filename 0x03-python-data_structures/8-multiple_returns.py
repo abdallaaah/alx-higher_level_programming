1455f79e015fdce4a2d821d14a808d4da0f5d6a3
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-20 19:28:55 -0700

    check

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (not sentence):
-        sentence[0] = None
-    return len(sentence), sentence[0]
+    sentencee = None
+    if sentence:
+        sentencee = sentence[0]
+    return len(sentence), sentencee
