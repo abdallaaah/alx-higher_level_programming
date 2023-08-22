c9a1bb3f5f4cdb07daedd9aa33a194beb753a026
abdalla <eng.abdallaeltawil@gmail.com>
2023-08-22 14:56:01 -0700

    hey task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     x = a_dictionary.get(key, 5)
     if x == 5:
-        ##update
         a_dictionary[key] = value
     else:
-        ##add new
         a_dictionary[key] = value
     return a_dictionary
