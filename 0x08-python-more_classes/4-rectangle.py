27f815c1ab066e73b8c8c1aa11d6138c910bdbe2
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-04 19:24:10 -0700

    check task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,6 +59,6 @@ class Rectangle:
                 rect.append("#")
             rect.append("\n")
         return ''.join(rect)
-    
+
     def __repr__(self):
         return f"Rectangle({self.width},{self.height})"
