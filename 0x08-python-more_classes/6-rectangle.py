38fa019c7e4d288035d85db57594b2c6017abfa8
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-05 16:39:57 -0700

    check this function

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle:
     """ class represent the rectange"""
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Create a new rectangel
         Args:
