2c157652b15379c76912d3f9a10be68ec539e33c
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-13 18:04:06 -0700

    cechk task 3

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" inheritence tasks"""
+
+
+def is_same_class(obj, a_class):
+    """is same class or not"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
