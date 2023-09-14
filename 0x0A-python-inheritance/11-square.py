107fb4bf628105e714550a51903b871181bd43f0
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-13 20:10:23 -0700

    cechl

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""how to inherit"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ square inherit from rectagel"""
+    def __init__(self, size):
+        """ init function"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    
+    def __str__(self):
+        """str function"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
