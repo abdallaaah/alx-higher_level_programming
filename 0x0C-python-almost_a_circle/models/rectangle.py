4eb70552da340a8a8fe71d97a19bba1028e41519
abdalla <eng.abdallaeltawil@gmail.com>
2023-09-18 18:00:07 -0700

    update the rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,6 +98,16 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """ return dectitonry with elemnsts"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y,
+            }
+
     def __str__(self):
         """Print the str"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
