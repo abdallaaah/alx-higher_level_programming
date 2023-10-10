b072c63432f69e516ebbf3473f62547165f9a1b3
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-09 20:23:03 -0700

    check

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""Defines a file-writing function."""
+""" overwrite function """
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    """ write with overwrite file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        number = f.write(text)
+        return number
