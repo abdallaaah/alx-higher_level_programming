1007c1577d65928961c00bd347ff333fcef3c476
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-10 19:08:10 -0700

    check dump

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """ from object ot json"""
+import json
 
 
 def to_json_string(my_obj):
     """ seralzation means encoding """
+    print('type of this object',type(my_obj))
     return json.dumps(my_obj)
