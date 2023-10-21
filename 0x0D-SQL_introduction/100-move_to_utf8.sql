ef93d267675d59dcfb16653f911250148682b43d
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-20 19:36:07 -0700

    check altering and utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -12,7 +12,6 @@ ALTER TABLE
 
 ALTER TABLE
     hbtn_0c_0.first_table
-    CHANGE name name
-    VARCHAR(191)
+    MODIFY COLUMN name VARCHAR(191)
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
