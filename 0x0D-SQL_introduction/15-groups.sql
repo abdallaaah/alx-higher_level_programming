b370507c421466441519e9b28a1521c4ad5cee73
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-20 19:14:49 -0700

    check group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 --  lists the number of records with the same score in the table
-SELECT score , COUNT(*) AS number FROM second_table group by score;
+SELECT score , COUNT(*) AS number FROM second_table group by score order by score DESC;
