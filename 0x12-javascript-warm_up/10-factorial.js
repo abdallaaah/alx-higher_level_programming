0ff3a21aee33d1b584d9532a4f89044f647b2af0
abdalla <eng.abdallaeltawil@gmail.com>
2023-11-15 01:39:36 -0800

    check

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 
-function factorialize(num) {
-  if (num === 0 || num === 1)
-    return 1;
-  for (var i = num - 1; i >= 1; i--) {
+function factorialize (num) {
+  if (num === 0 || num === 1) { return 1; }
+  for (let i = num - 1; i >= 1; i--) {
     num *= i;
   }
   return num;
 }
-console.log(factorialize(process.argv[2]));
+if (process.argv[2] == null) {
+  console.log(1);
+} else {
+  console.log(factorialize(process.argv[2]));
+}
