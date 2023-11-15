465f98f8c067b40106c2ff26c924816cfc0169db
abdalla <eng.abdallaeltawil@gmail.com>
2023-11-14 23:55:31 -0800

    check this

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let = x
+if (process.argv.length === 2) { 
+console.log('Not a number');
+} else {
+x = parseInt(argv[2], 10);
+if (!isNaN(x)) {
+console.log(x);
+} else {
+console.log('Not a number');
+}
