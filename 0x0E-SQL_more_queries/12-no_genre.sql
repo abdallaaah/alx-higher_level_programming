2b5bb3017486f7a8a37ea52f2e015dd254efee17
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-10 04:27:07 -0800

    checcc

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@ SELECT t.title, g.genre_id
 FROM tv_shows AS t 
 LEFT JOIN tv_show_genres AS g ON g.show_id = t.id
 WHERE g.genre_id IS NULL
-ORDER BY g.show_id, t.id ASC;
+ORDER BY g.show_id, t.title ASC;
