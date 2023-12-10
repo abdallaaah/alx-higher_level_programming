36b9f8abfcc68dbfb8a8fb3ba48a0da9e382e922
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-10 03:50:04 -0800

    cgec

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT t.title, g.show_id
+SELECT t.title, g.genre_id
 FROM tv_shows AS t JOIN tv_show_genres AS g
 ON g.show_id = t.id
-WHERE g.show_id > 0;
+WHERE g.genre_id > 0;
