e8770af532d9f6cdf00655649a9846f534b3bc64
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-10 04:07:00 -0800

    check newee

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- 11. Genre ID for all shows
 SELECT t.title, g.genre_id
-FROM tv_shows AS t RIGHT JOIN tv_show_genres AS g
+FROM tv_shows AS t LEFT JOIN tv_show_genres AS g
 ON g.show_id = t.id
 ORDER BY t.title, g.genre_id ASC;
