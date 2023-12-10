98dcf8f6dc30e2ad03bcdbcd1a1010cf7bdff6b3
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-10 06:10:29 -0800

    check this again

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- My genres
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE title = "Dexter"
+ORDER BY name ASC;
