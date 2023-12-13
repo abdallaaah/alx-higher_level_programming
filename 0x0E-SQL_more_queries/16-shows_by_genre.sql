4d82a6f104247ebef8b93b26a951e48354b45733
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-13 00:24:05 -0800

    check this

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List shows and genres
+SELECT tv_shows.title, g.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres AS g ON tv_show_genres.genre_id = g.id
+
