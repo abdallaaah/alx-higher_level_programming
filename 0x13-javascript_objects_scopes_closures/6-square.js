837d78b10182df994270bd6bc4ecc2d4706864b6
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-12 18:22:19 -0800

    check this

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    for (let x = 0; x < this.size; x++) {
+      let z = '';
+      for (let y = 0; y < this.size; y++) {
+        if (!c) {
+          z += ('X');
+        } else {
+          z += ('C');
+        }
+      }
+      console.log(z);
+    }
+  }
+}
+module.exports = Square;
