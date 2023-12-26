2db78da85d77a8b12c930bf0eaecfdf2f39dd539
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-25 21:05:32 -0800

    check where ariozna

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(**connection_string)
     cur = conn.cursor()
     name = sys.argv[4]
-    cur.execute(f"SELECT * FROM states WHERE name='{name}' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(name))
     result = cur.fetchall()
     for row in result:
         print(row)
