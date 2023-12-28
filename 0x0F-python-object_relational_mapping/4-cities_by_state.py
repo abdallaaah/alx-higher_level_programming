36806fb3ad93ac51dc959e6b4675cd5d0d44b277
abdallaaah <abdallah.hesham@msa.edu.eg>
2023-12-27 20:39:35 -0800

    check join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" execute with join statemt"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """ the main"""
+    connection_string = {
+            'host': 'localhost',
+            'user': sys.argv[1],
+            'passwd': sys.argv[2],
+            'port': 3306,
+            'db': sys.argv[3],
+            'charset': 'utf8'
+            }
+    con = MySQLdb.connect(**connection_string)
+    cur = con.cursor()
+    command = "SELECT c.state_id,c.name,s.name FROM states AS s join cities AS c ON c.state_id = s.id ORDER BY c.id ASC"
+    cur.execute(command)
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    con.close()
