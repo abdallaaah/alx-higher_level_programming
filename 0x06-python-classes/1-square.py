7f21f28d714c3bfb2da463e6da9562a68b1417e0
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-02-21 14:36:46 -0800

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module is a test of the class"""
+
+
+class Square:
+    """tis is a basic square"""
+
+    def __init__(self, size):
+        """initalize function"""
+
+        self.__size = size
