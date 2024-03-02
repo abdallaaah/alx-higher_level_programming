c79b89457a5702aebe31b3d99d5f711d6058efa9
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-02 13:06:44 -0800

    check square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+from . import  rectangle
+
+class Square(rectangle.Rectangle):
+    """square class that inherit from rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """return the human redabile"""
+        return f"[Square] {self.id} {self.x}/{self.y} - {self.size}"
+
+
+
