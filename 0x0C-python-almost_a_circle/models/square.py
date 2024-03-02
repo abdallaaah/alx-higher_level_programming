fe211b109c1d142c5e92d3f5e884746904cdb494
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-02 14:08:35 -0800

    check this

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,9 +6,23 @@ from . import rectangle
 class Square(rectangle.Rectangle):
     """square class that inherit from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
-        super().__init__(size, size, x=x, y=y, id=id)
+        self.__size = size
+        super().__init__(size, size, x, y, id=id)
 
     def __str__(self):
         """return the human redabile"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+
+    @property
+    def size(self):
+        """return size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sit the size"""
+
+        self.width = size
+        self.height = size
