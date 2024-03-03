8328dd862bfea444beeb085748aac1b1ba4ec1b2
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-03 01:32:32 -0800

    update the update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,13 +6,11 @@ from . import rectangle
 class Square(rectangle.Rectangle):
     """square class that inherit from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.__size = size
         super().__init__(size, size, x, y, id=id)
 
     def __str__(self):
         """return the human redabile"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
@@ -26,3 +24,26 @@ class Square(rectangle.Rectangle):
 
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """update the square from args and if not from kwargs"""
+
+        if args:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+                self.height = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs.get('id')
+            if 'size' in kwargs:
+                self.width = kwargs.get('size')
+                self.height = kwargs.get('size')
+            if 'x' in kwargs:
+                self.x = kwargs.get('x')
+            if 'y' in kwargs:
+                self.y = kwargs.get('y')
