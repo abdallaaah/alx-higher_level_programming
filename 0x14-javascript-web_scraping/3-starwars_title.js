65354bdf898f48dc4c27188efc5aaf0f252622d7
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-26 18:52:49 -0700

    check me

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// this how to parse data body
 const id = process.argv[2];
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, function(error, response, body) {
-    if (error) {
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, function (error, response, body) {
+  if (error) {
     console.log(error);
     return;
-    }
-    const filmdata = JSON.parse(body);
-    console.log(filmdata.title);
+  }
+  const filmdata = JSON.parse(body);
+  console.log(filmdata.title);
 });
