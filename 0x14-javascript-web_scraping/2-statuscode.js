6c607110b2efe52a26482e0389a371083b34e557
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-05-15 17:05:28 -0700

    check status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// proint the status code
+// get request for url and print the status code
 
 const request = require('request');
 const url = process.argv[2];
@@ -7,7 +7,7 @@ request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 }
 );
