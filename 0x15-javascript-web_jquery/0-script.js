953af055a3d7db861d63f0456df4bb46b25943e6
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-05-26 16:17:35 -0700

    check there no thing

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,1 +1,12 @@
-document.querySelector('HEADER').style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function (eventtt){
+    const header = document.querySelector('header');
+    if (header){
+        header.style.color = "#FF0000";
+    }
+    else{
+        console.log('there is no head tag');
+    }
+})
+
+
+
